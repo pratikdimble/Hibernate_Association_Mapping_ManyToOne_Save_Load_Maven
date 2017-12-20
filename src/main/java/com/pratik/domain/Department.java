2eaa58a4734122ca8cc8eedbb7660ca3a5ac5e8d
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-20 09:15:55 -0800

    Add files via upload

---
+++ src/main/java/com/pratik/domain/Department.java
@@ -0,0 +1,29 @@
+package com.pratik.domain;
+
+public class Department {
+	
+	private int deptId;
+	private String deptName;
+	
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Department [deptId=" + deptId + ", deptName=" + deptName + "]";
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/pratik/domain/Employee.java
@@ -0,0 +1,39 @@
+package com.pratik.domain;
+
+public class Employee {
+
+	private int eid;
+	private String ename;
+	private float esal;
+	private Department dept;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public float getEsal() {
+		return esal;
+	}
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+	public Department getDept() {
+		return dept;
+	}
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", esal=" + esal + ", dept=" + dept + "]";
+	}
+
+	
+}
